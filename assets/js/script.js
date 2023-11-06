9547cf31978ccb7f824e1bc63fcf46957685894e
Code-Monster1 <usupovaliakbar01@gmail.com>
2023-11-06 23:21:53 +0600

    add banner block

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,3 +1,5 @@
+// Hamburger
+
 let hamburger = document.querySelector('.hamburger-menu')
 let navCollapse = document.querySelector('.nav__collapse')
 hamburger.addEventListener('click', () => {
@@ -5,8 +7,29 @@ hamburger.addEventListener('click', () => {
   navCollapse.classList.toggle('active')
 });
 
+// Language dropdown
+
 let lang = document.querySelector('.lang')
 let langDropDown = document.querySelector('.lang__dropdown')
 lang.addEventListener('click', () => {
   langDropDown.classList.toggle('active')
 });
+
+// Open Modal
+
+document.getElementById("openModal").addEventListener("click", function() {
+  document.getElementById("modal").style.display = "block";
+
+  document.getElementById("modal").addEventListener("click", function(event) {
+    if (event.target === this) {
+      closeModal();
+    }
+  });
+});
+
+document.getElementById("closeModal").addEventListener("click", closeModal);
+
+function closeModal() {
+  document.getElementById("modal").style.display = "none";
+  document.getElementById("modal").removeEventListener("click", closeModal);
+}
